310e7361713c939491c8c97855ebe552505f7cff
Taiki Kuwahara <kkkuwaaa@TaikinoMac-mini.local>
2024-12-05 20:56:35 +0900

    update HomeView

--- manager/HomeView.swift
+++ manager/HomeView.swift
@@ -3,6 +3,8 @@ import SwiftUI
 struct HomeView: View {
     @State private var isShowingAddExpenseView = false
     @State private var balanceText: String = "計算中..."
+    @State private var payfrom: String = "くわ"
+    @State private var payto: String = "はな"
     
     @AppStorage("user1Name") private var user1Name: String = "Person 1"
     @AppStorage("user2Name") private var user2Name: String = "Person 2"
@@ -10,58 +12,99 @@ struct HomeView: View {
 
     var body: some View {
         NavigationView {
-            VStack(spacing: 20) {
-                Text("ホーム画面")
-                    .font(.largeTitle)
-                    .padding()
-                
-                Text(balanceText)
-                    .font(.headline)
-                    .padding()
-                
-                // 更新ボタン
-                Button("更新") {
+            ZStack{
+                Color.mint
+                    .ignoresSafeArea()
+                VStack(spacing: 20) {
+                    ZStack{
+                        Color.white
+                        VStack{
+                            HStack{
+                                Text(payfrom)
+                                    .font(.system(size: 35))
+                                    .padding(10)
+                                Image(systemName: "arrowshape.right.fill")
+                                    .font(.system(size: 30))
+                                Text(payto)
+                                    .font(.system(size: 35))
+                                    .padding(10)
+                            }
+                            .padding()
+                            HStack{
+                                Text(balanceText)
+                                    .font(.system(size: 35))
+                                    .font(.headline)
+                                Button(action: {
+                                    calculateBalances()
+                                }) {
+                                    Image(systemName: "arrow.trianglehead.counterclockwise.rotate.90")
+                                        .font(.system(size: 15))
+                                        .foregroundColor(.white)
+                                        .padding(5)
+                                        .background(Color.gray)
+                                        .cornerRadius(100)
+                                        .padding(7)
+                                }
+
+                            }
+                            .padding()
+                        }
+                    }
+                    .frame(width: 320, height:250)
+                    .cornerRadius(10)
+                    
+                    Button("支出を追加/精算") {
+                        isShowingAddExpenseView = true
+                    }
+                    .font(.system(size:25))
+                    .frame(width: 320, height: 120)
+                    .background(Color.pink)
+                    .foregroundStyle(.white)
+
+                    .cornerRadius(10)
+                    .sheet(isPresented: $isShowingAddExpenseView) {
+                        AddExpenseView()
+                    }
+                    HStack{
+                        NavigationLink(destination: ListView()) {
+                            Image(systemName: "book")
+                                .font(.system(size:40))
+                                .frame(width: 124, height: 80)
+                                .buttonStyle(.borderedProminent)
+                                .padding()
+                                .background(Color.blue)
+                                .foregroundStyle(.white)
+                                .cornerRadius(10)
+                        }
+                        
+                        NavigationLink(destination: OptionView()) {
+                            Image(systemName: "gearshape")
+                                .font(.system(size:40))
+                                .frame(width: 124, height: 80)
+                                .buttonStyle(.borderedProminent)
+                                .padding()
+                                .background(Color.blue)
+                                .foregroundStyle(.white)
+                                .cornerRadius(10)
+                        }
+                    }
+                }
+                .onAppear {
                     calculateBalances()
                 }
-                .buttonStyle(.bordered)
-                .padding()
-
-                Button("追加") {
-                    isShowingAddExpenseView = true
-                }
-                .buttonStyle(.borderedProminent)
-                .padding()
-                .sheet(isPresented: $isShowingAddExpenseView) {
-                    AddExpenseView()
-                }
-
-                NavigationLink(destination: ListView()) {
-                    Text("家計簿一覧")
-                        .buttonStyle(.borderedProminent)
-                        .padding()
-                }
-                
-                NavigationLink(destination: OptionView()) {
-                    Text("設定")
-                        .buttonStyle(.borderedProminent)
-                        .padding()
-                }
-            }
-            .onAppear {
-                calculateBalances()
             }
         }
+        .background(Color.red)
     }
 
     /// 支払い金額の計算
     private func calculateBalances() {
-        print("計算結果を行います。")
         balanceText = "計算中..." // 更新前に一旦メッセージをリセット
 
         // householdIDが空でないか確認
         guard !householdID.isEmpty else {
             DispatchQueue.main.async {
-                balanceText = "家計簿IDが設定されていません。"
+                balanceText = "20000"
             }
             return
         }
@@ -71,26 +114,33 @@ struct HomeView: View {
             var user2Total = 0
 
             for expense in expenses {
-                let amount = expense.paymentType == "立て替え" ? expense.amount * 2 : expense.amount
+                let amount = (expense.paymentType == "立て替え" || expense.paymentType == "精算") ? expense.amount * 2 : expense.amount
                 
                 if expense.person == user1Name {
                     user1Total += amount
                 } else if expense.person == user2Name {
                     user2Total += amount
                 }
-                print(expense.person)
-                print(amount)
             }
 
             DispatchQueue.main.async {
                 if user1Total > user2Total {
-                    balanceText = "\(user2Name)から\(user1Name)に\(user1Total - user2Total)円"
+                    payfrom = user2Name
+                    payto = user1Name
+                    balanceText = "\((user1Total - user2Total)/2)円"
                 } else if user2Total > user1Total {
-                    balanceText = "\(user1Name)から\(user2Name)に\(user2Total - user1Total)円"
+                    payfrom = user1Name
+                    payto = user2Name
+                    balanceText = "\((user2Total - user1Total)/2)円"
                 } else {
-                    balanceText = "二人の支払い額は同じです"
+                    balanceText = "Thank You!"
                 }
             }
         }
+    }
+}
+struct AddExpenseView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
     }
 }
